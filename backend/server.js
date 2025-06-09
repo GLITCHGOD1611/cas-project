28eb505bb268e5e2f09fec6a54c33806e7a56181
GLITCHGOD <omkardeshmane832@gmail.com>
2025-06-09 22:41:54 +0530

    u service js

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
